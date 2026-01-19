adee438dc5aaf97037de113bf8286e0d1f9b3230
zh4ngyj <zh4ngyj@gmail.com>
2026-01-19 17:28:39 +0800

    fix

--- langchain_tutorial/langchain_demo.py
+++ langchain_tutorial/langchain_demo.py
@@ -35,13 +35,13 @@ async def main():
 
     # 调用工具
     try:
-        excel_response = await agent.ainvoke(
+        result = await agent.ainvoke(
             {"messages": [
                     {"role": "system", "content": "你是一个乐于助人的助手，擅长使用提供的工具解决问题，回答要简洁明了。"},
                     {"role": "user", "content": "创建一个excel：文件名称为“test”，内容为[['name','age'],['Bob','20'],['张三','23']]"}
                 ]}
         )
-        print("工具调用结果:", excel_response)
+        print("完整返回结果:", result)
     except Exception as e:
         print(f"工具调用失败: {e}")
 
